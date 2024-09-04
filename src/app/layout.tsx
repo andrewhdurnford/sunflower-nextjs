8a8feff0f7619e890cb09897276787c42d22d567
Andrew Durnford <andrewhdurnford@gmail.com>
2024-09-04 02:24:22 -0400

    made status bar clear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-// Import the Google font 'Inter'
 const inter = Inter({ subsets: ["latin"] });
 
-// Define the metadata for SEO and accessibility
 export const metadata: Metadata = {
   title: "Sunflower Capital",
   description: "We invest at the earliest stages in companies building foundational picks and shovels infrastructure.",
@@ -19,8 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="theme-color" content="#FFF9DE" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="transparent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
