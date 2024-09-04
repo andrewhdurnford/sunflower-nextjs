a768ea317b5717785c96c39a8b94efa84a77478d
Andrew Durnford <andrewhdurnford@gmail.com>
2024-09-04 02:26:03 -0400

    status bar translucent:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="theme-color" content="transparent" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
