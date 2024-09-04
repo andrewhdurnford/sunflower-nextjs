4cc7404d42342486ec54757c5dc69798ed8f7854
Andrew Durnford <andrewhdurnford@gmail.com>
2024-09-04 01:24:26 -0400

    changed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sunflower Capital",
-  description: "",
+  description: "We invest at the earliest stages in companies building foundational picks and shovels infrastructure.",
 };
 
 export default function RootLayout({
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
