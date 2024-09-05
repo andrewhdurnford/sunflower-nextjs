7e1d57dc9cc875a68b607ef00d3e1cda2795cd2c
Andrew Durnford <andrewhdurnford@gmail.com>
2024-09-05 11:35:12 -0400

    testing mobile sizing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="theme-color" content="#FFF9DE" />
         {/* <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" /> */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="initial-scale=0.9, width=device-width, height=device-height, viewport-fit=cover, user-scalable=no" />
       </head>
       <body className={inter.className}>
         <main>{children}</main>
