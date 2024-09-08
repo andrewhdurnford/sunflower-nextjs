128a850b442f92ab75020a02bc0f25875bebcb81
Andrew Durnford <andrewhdurnford@gmail.com>
2024-09-07 23:27:39 -0400

    fixed quotation marks

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -177,7 +177,7 @@ const Testimonials: React.FC = () => {
           {quotes.map((quote, index) => (
             <li key={index} className="glide__slide flex flex-col justify-center items-center gap-6">
               <div className="font-bitter text-base sm:text-xl xl:text-2xl w-full text-left sm:leading-extra-loose md:leading-extra-loose lg:leading-extra-loose xl:leading-extra-loose">
-                "{quote.quote}"
+              &ldquo;{quote.quote}&rdquo;
               </div>
               <div className="font-bitter text-base sm:text-xl md:text-2xl text-dark-green text-left w-full">
               &emsp;— {quote.author}, <span className="font-bitter-italic">{quote.company}</span>
