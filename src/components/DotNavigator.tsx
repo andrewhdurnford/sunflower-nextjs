5614a8be8b769b5bbe0e87e66c7d19bd73029764
Andrew Durnford <andrewhdurnford@gmail.com>
2025-04-22 19:27:42 -0400

    feat: changed dots to sunflowers

--- src/components/DotNavigator.tsx
+++ src/components/DotNavigator.tsx
@@ -12,31 +12,33 @@ const DotNavigator: React.FC<DotNavigatorProps> = ({ currentScreen, onDotClick, 
     return (
         <div className="dot-container flex gap-2">
             {Array.from({ length: totalScreens }).map((_, index) => {
-                const dotClass = (() => {
-                    if (index === currentScreen) {
-                        return currentScreen === 0
-                            ? 'bg-offwhite scale-125'
-                            : 'bg-dark-green scale-125';
-                    } else {
-                        if (currentScreen === 0) {
-                            return isMobile
-                                ? 'bg-offwhite opacity-20 scale-125'
-                                : 'bg-offwhite opacity-20 scale-75';
-                        } else {
-                            return isMobile
-                                ? 'bg-dark-green opacity-20 scale-125'
-                                : 'bg-dark-green opacity-20 scale-75';
-                        }
-                    }
-                })();
+            const dotClass = (() => {
+                // if (index === currentScreen) {
+                // return currentScreen === 0
+                //     ? 'bg-offwhite scale-125'
+                //     : 'bg-dark-green scale-125';
+                // } else {
+                // if (currentScreen === 0) {
+                //     return isMobile
+                //     ? 'bg-offwhite opacity-20 scale-125'
+                //     : 'bg-offwhite opacity-20 scale-75';
+                // } else {
+                //     return isMobile
+                //     ? 'bg-dark-green opacity-20 scale-125'
+                //     : 'bg-dark-green opacity-20 scale-75';
+                // }
+                // }
+            })();
 
-                return (
-                    <div
-                        key={`${isMobile}-${index}`}
-                        className={`dot transform ${dotClass} transition-transform duration-300`}
-                        onClick={() => onDotClick(index)}
-                    ></div>
-                );
+            return (
+                <img
+                key={`${isMobile}-${index}`}
+                src={'/images/dot.svg'}
+                alt={`dot ${index}`}
+                className={`dot transform ${dotClass} transition-transform duration-300 hover:cursor-pointer ${currentScreen === index ? 'scale-150' : 'hover:scale-125'}`}
+                onClick={() => onDotClick(index)}
+                />
+            );
             })}
         </div>
     );
